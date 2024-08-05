c82171fac7b7ae9f6811b33fc935333666fc94ee
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2024-08-06 00:16:12 +0700

    Add test cases

---
+++ tests/TimezonesListTest.php
@@ -0,0 +1,45 @@
+<?php
+
+it('can get a grouped array of timezones', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $this->assertIsArray($timezones->toArray(grouped: true));
+});
+
+it('can get a grouped array of timezones with html entities', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $this->assertIsArray($timezones->toArray(grouped: true, htmlencode: true));
+});
+
+it('can get a grouped array of timezones without html entities', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $this->assertIsArray($timezones->toArray(grouped: true, htmlencode: false));
+});
+
+it('can get a flat array of timezones', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $this->assertIsArray($timezones->toArray(grouped: false));
+});
+
+it('can exclude continents', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $excludedTimezones = $timezones->excludeContinents(['Africa', 'America'])->toArray(grouped: false);
+
+    $this->assertIsArray($excludedTimezones);
+    $this->assertArrayNotHasKey('Africa', $excludedTimezones);
+    $this->assertArrayNotHasKey('America', $excludedTimezones);
+});
+
+it('can include general timezones', function () {
+    $timezones = new \Soap\TimezonesList\TimezonesList;
+
+    $includedTimezones = $timezones->includeGeneral()->toArray(grouped: false);
+
+    $this->assertIsArray($includedTimezones);
+    $this->assertArrayHasKey('UTC', $includedTimezones);
+    $this->assertArrayHasKey('GMT', $includedTimezones);
+});
